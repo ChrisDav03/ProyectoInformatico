df0c0c76ed596951eea478b68ad304c429ee38eb
Juan Bohorquez <juan_david.bohorquez@uao.edu.co>
2024-03-07 16:41:32 -0500

    Creating components Sign In and Sign Up, change index name

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from '../src/adapters/primary/ui/components/App'
 import './index.css'
+// import SignIn from './adapters/primary/ui/components/SignIn'
+import SignUp from './adapters/primary/ui/components/SignUp'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SignUp />
   </React.StrictMode>,
 )
